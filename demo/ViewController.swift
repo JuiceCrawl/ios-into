d15784aa86aab39b5bcaddaa6847ae56a520963f
JuiceCrawl <annaegarcia@gmail.com>
2016-01-16 20:55:59 -0500

    initial commit

--- demo/ViewController.swift
+++ demo/ViewController.swift
@@ -9,10 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var btnSubmit: UIButton!
+    @IBOutlet var nameField: UITextField!
+    @IBOutlet var labelField: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("viewDidLoad")
+//        self.view.backgroundColor = UIColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 1.0)
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func btnSubmitAction(){
+        //anna's atend that works but less efficient
+        //print("\(self.nameField.text!)")
+        //labelField.self.text = "\(self.nameField.text!)"
+        
+        //dan's code
+        let name = self.nameField.text!
+        print("\(name)")
+        self.labelField.text = self.nameField.text
     }
 
     override func didReceiveMemoryWarning() {
